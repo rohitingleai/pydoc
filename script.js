af01a46b938e365f16d3900edf8433bbaf6dfc2f
rohitingleai <144927273+rohitingleai@users.noreply.github.com>
2023-09-14 22:17:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ menuBtn.addEventListener("click", function () {
     sidebarLeft.classList.toggle("active");
 });
 
+
+
+
+
 var form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -13,3 +17,25 @@ form.addEventListener("submit", function (e) {
     search.value = "site:www.ristudypost.com " + search.value;
     form.submit();
 });
+
+
+
+
+
+
+// Disable right-click context menu
+        window.addEventListener('contextmenu', function (e) {
+            e.preventDefault();
+        });
+
+        // Disable copy and cut keyboard shortcuts
+        window.addEventListener('keydown', function (e) {
+            if (e.ctrlKey && (e.key === 'c' || e.key === 'x')) {
+                e.preventDefault();
+            }
+        });
+
+        // Detect Chrome extension activity
+        if (typeof chrome !== 'undefined' && chrome.runtime && chrome.runtime.id) {
+            alert("Chrome extension detected. Some functionality may be restricted.");
+        }
